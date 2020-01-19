2cd6588febf401852e5886f4c90dc50bd826f26f
GASSY1985 <freefeeling_freesence@i.softbank.jp>
2020-01-19 21:42:57 +0900

    update:0119_2140

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root "blogs#index"
   resources :users, only: [:index, :edit, :update, :new, :create] do
     resources :blogs, only: [:index, :new, :create, :edit, :update]
+    resources :blog_edit, only: [:new, :create, :edit, :update,:delete]
     resources :chats, only: [:index, :new, :create, :edit, :update] do
     end
   end
