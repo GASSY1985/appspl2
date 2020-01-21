9abca1f60985e0e06926d2a3d7d626177595e8c1
GASSY1985 <freefeeling_freesence@i.softbank.jp>
2020-01-21 19:41:37 +0900

    update:0121_1950

---
+++ app/controllers/write_controller.rb
@@ -0,0 +1,6 @@
+class WriteController < ApplicationController
+
+  def new
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   root "blogs#index"
   resources :users, only: [:index, :edit, :update, :new, :create] do
     resources :blogs, only: [:index, :new, :create, :edit, :update]
+    resources :write, only: [:index, :new, :create, :edit, :update]    
     resources :chats, only: [:index, :new, :create, :edit, :update] do
     end
   end
+
+
 end
