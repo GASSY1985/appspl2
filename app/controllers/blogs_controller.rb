4ad25df3d127d0389aa775410c4619d7c1b9e261
GASSY1985 <freefeeling_freesence@i.softbank.jp>
2020-01-24 21:24:22 +0900

    update:0124_2130

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,22 +1,28 @@
 class BlogsController < ApplicationController
+
   def index
   end
 
   def new
+    @blogs = Blog.new
   end
 
   def create
-    Blog.create(create_params)
-    redirect_to controller: :blogs, action: :index
+    @blog = current_user.blogs.new(create_params)
+      if @blog.save!
+        redirect_to controller: :blogs, action: :index ,notice: "投稿完了しました"        
+      else
+        render :new        
+      end
+  end
+
+  def show
+    @blog = Blog.find(params[:id])
+    # chatの繋ぎ方（includes chats? no method error出た）
   end
 
   private
   def create_params
     params.require(:blog).permit(:title, :content).merge(user_id: current_user.id)
   end
-
-  def show
-    # @blog = Blog.find(params[:id]).inculudes(:chats)
-  end
-
 end
